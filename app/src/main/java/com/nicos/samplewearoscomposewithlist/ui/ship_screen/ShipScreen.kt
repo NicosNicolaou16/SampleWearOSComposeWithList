c4138adc2e3f69a39fb1cefa90e76be26d9fe3c2
Nicos Nicolaou <nicosnicos16@gmail.com>
2023-10-28 14:01:41 +0300

    fix some UI issues for details screen

--- app/src/main/java/com/nicos/samplewearoscomposewithlist/ui/ship_screen/ShipScreen.kt
+++ app/src/main/java/com/nicos/samplewearoscomposewithlist/ui/ship_screen/ShipScreen.kt
@@ -1,26 +1,30 @@
 package com.nicos.samplewearoscomposewithlist.ui.ship_screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.wear.compose.material.Text
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
+import com.nicos.samplewearoscomposewithlist.R
 import kotlinx.coroutines.flow.asSharedFlow
 
 const val SHIP_ID_KEY = "ship_id_key"
@@ -37,13 +41,19 @@ fun ButtonScreen(
     Box(
         modifier = modifier
             .fillMaxSize()
-            .background(color = Color.Red),
+            .background(color = Color.Black),
         contentAlignment = Alignment.Center,
     ) {
         GlideImage(
             model = shipsModel?.image ?: "",
             contentDescription = null,
-            contentScale = ContentScale.Inside,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(CircleShape)
+                .border(2.dp, Color.Gray, CircleShape),
+            loading = placeholder(R.drawable.ic_baseline_image_24),
+            failure = placeholder(R.drawable.ic_baseline_image_24),
+            contentScale = ContentScale.Crop,
         )
         Text(
             text = shipsModel?.ship_name ?: "",
@@ -51,8 +61,10 @@ fun ButtonScreen(
             modifier = modifier.padding(top = 100.dp),
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Bold,
-            fontSize = 21.sp,
-            color = Color.Green
+            fontSize = 19.sp,
+            color = Color.Green,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
         )
     }
 }
