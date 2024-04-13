f2c82a4153afc6e43bb22ace3f3b54ba97ce1d2f
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-04-14 01:59:01 +0300

    update the Gradle and Kotlin to latest version, update some dependencies libraries and update README file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
@@ -63,18 +63,18 @@ android {
 
 val coroutineVersion by extra("1.8.0")
 val composeHiltNavigationVersion by extra("1.2.0")
-val hiltVersion by extra("2.51")
+val hiltVersion by extra("2.51.1")
 val hiltCompilerVersion by extra("1.2.0")
 val activityVersion by extra("1.8.2")
 val fragmentVersion by extra("1.6.2")
-val retrofitVersion by extra("2.9.0")
+val retrofitVersion by extra("2.11.0")
 val okHttpVersion by extra("4.11.0")
 val roomVersion by extra("2.6.1")
 val glideVersion by extra("4.16.0")
 val glideComposeVersion by extra("1.0.0-beta01")
 val materialDesignVersion by extra("1.11.0")
 val multidexVersion by extra("2.0.1")
-val wearComposeVersion by extra("1.3.0")
+val wearComposeVersion by extra("1.3.1")
 val activityComposeVersion by extra("1.8.2")
 
 dependencies {
@@ -83,7 +83,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     //Compose
     implementation("androidx.activity:activity-compose:$activityComposeVersion")
-    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -120,7 +120,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
