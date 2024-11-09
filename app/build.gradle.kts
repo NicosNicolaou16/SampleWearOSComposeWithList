648cef875d15867356062bdb5c30d32ff1171fa7
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-11-10 00:06:24 +0200

    update Target SDK from 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
 
     defaultConfig {
         applicationId = "com.nicos.samplewearoscomposewithlist"
-        minSdk = 27
-        targetSdk = 34
+        minSdk = 28
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
