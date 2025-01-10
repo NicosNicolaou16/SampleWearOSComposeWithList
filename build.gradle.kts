82e2a869d019925f4c2cd42489f23aa41be97a58
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-01-11 01:12:19 +0200

    update libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id("com.android.application") version "8.8.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("com.google.dagger.hilt.android") version "2.55" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
