4cb1cc53597b1f56805bccdab93e8502b43cf1ff
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-16 01:44:19 +0300

    update Target SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,13 +8,13 @@ plugins {
 
 android {
     namespace = "com.nicos.samplewearoscomposewithlist"
-    compileSdk = 35
-    buildToolsVersion = "35.0.1"
+    compileSdk = 36
+    buildToolsVersion = "36.0.0"
 
     defaultConfig {
         applicationId = "com.nicos.samplewearoscomposewithlist"
-        minSdk = 28
-        targetSdk = 35
+        minSdk = 29
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
