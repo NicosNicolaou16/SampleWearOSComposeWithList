ed25170e54abf443ebccf89694bea0b4dc1992bc
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-03-28 00:52:49 +0200

    update KSP version to 2.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("com.android.application") version "9.1.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.20" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
-    id("com.google.devtools.ksp") version "2.3.4" apply false
+    id("com.google.devtools.ksp") version "2.3.6" apply false
 }
