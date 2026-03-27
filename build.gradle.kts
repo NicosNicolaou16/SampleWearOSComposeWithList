762245d59f033454d048ff6676fa5433299b11b0
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-03-28 00:38:35 +0200

    update Hilt version to 2.59.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id("com.android.application") version "9.1.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
-    id("com.google.dagger.hilt.android") version "2.57.2" apply false
+    id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("com.google.devtools.ksp") version "2.3.4" apply false
 }
